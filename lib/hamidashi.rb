e31d9581faf05bc7f61df436ae22d8730c90611f
Seiei Miyagi <hanachin@gmail.com>
2015-06-12 12:27:37 +0900

    はみだしてるか調べる部分をHamidashiクラスに移動

--- lib/hamidashi.rb
+++ lib/hamidashi.rb
@@ -1,5 +1,32 @@
 require "hamidashi/version"
 
-module Hamidashi
-  # Your code goes here...
+class Hamidashi
+  def てる?(pdf_path, page)
+    colornum=`convert -density 92 -geometry #{width} -crop #{crop} "#{pdf_path}[#{page}]" -format %c histogram:info: | wc -l`.to_i
+    colornum != expected_colornum
+  end
+
+  def てる画像を保存(pdf_path, page, path)
+    `convert -density 92 -geometry #{width} "#{pdf_path}[#{page}]" #{path}`
+  end
+
+  private
+
+  # TODO 外部から設定出来るようにしたい
+
+  def crop
+    "#{crop_width}x+#{width - crop_width}+0"
+  end
+
+  def crop_width
+    28
+  end
+
+  def expected_colornum
+    1
+  end
+
+  def width
+    640
+  end
 end
--- lib/hamidashi/version.rb
+++ lib/hamidashi/version.rb
@@ -1,3 +1,3 @@
-module Hamidashi
+class Hamidashi
   VERSION = "0.1.0"
 end
