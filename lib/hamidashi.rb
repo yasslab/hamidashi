810856b2c0115d8a8a64dba2f2604229f4a1ced0
Seiei Miyagi <hanachin@gmail.com>
2015-07-17 16:34:09 +0900

    Change Hamidashi#initialize, #overflow?, #save_page, #save_preview_page interface

--- lib/hamidashi.rb
+++ lib/hamidashi.rb
@@ -2,21 +2,22 @@ require "hamidashi/version"
 require "shellwords"
 
 class Hamidashi
-  def initialize(percentage)
+  def initialize(percentage, pdf_path)
     @crop_width = (width * percentage / 100).to_i
+    @pdf_path   = pdf_path
   end
 
-  def overflow?(pdf_path, page)
-    colornum=`convert -density 92 -geometry #{width} -crop #{crop} "#{pdf_path.shellescape}[#{page.to_s.shellescape}]" -format %c histogram:info: | wc -l`.to_i
+  def overflow?(page)
+    colornum=`convert -density 92 -geometry #{width} -crop #{crop} "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" -format %c histogram:info: | wc -l`.to_i
     colornum != expected_colornum
   end
 
-  def save_page(pdf_path, page, path)
-    `convert -density 92 -geometry #{width} "#{pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
+  def save_page(page, path)
+    `convert -density 92 -geometry #{width} "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
   end
 
-  def save_preview_page(pdf_path, page, path)
-    `convert -density 92 -geometry #{width} -fill "rgba(0, 255, 0, 50%)" -strokewidth 0 -draw "rectangle #{width - crop_width},#{height},#{width},0" "#{pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
+  def save_preview_page(page, path)
+    `convert -density 92 -geometry #{width} -fill "rgba(0, 255, 0, 50%)" -strokewidth 0 -draw "rectangle #{width - crop_width},#{height},#{width},0" "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
   end
 
   private
