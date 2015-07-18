7b5c8cf6bde31520097fcb159d8c3f9d1d6739a5
Seiei Miyagi <hanachin@gmail.com>
2015-07-19 04:33:00 +0900

    Use RMagick instead of exec ImageMagick command

--- hamidashi.gemspec
+++ hamidashi.gemspec
@@ -26,8 +26,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "pdf-reader"
   spec.add_dependency "docopt"
+  spec.add_dependency "pdf-reader"
+  spec.add_dependency "rmagick"
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "cucumber"
   spec.add_development_dependency "minitest"
--- lib/hamidashi.rb
+++ lib/hamidashi.rb
@@ -1,48 +1,67 @@
 require "hamidashi/version"
+require "RMagick"
 require "shellwords"
 
 class Hamidashi
+  attr_reader :percentage
+
   def initialize(percentage, pdf_path)
-    @crop_width = (width * percentage / 100).to_i
+    @percentage = percentage / 100r
     @pdf_path   = pdf_path
   end
 
   def overflow?(page)
-    histogram_info = `convert -density 92 -geometry #{width} -crop #{crop} "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" -format %c histogram:info:`
-    colornum = histogram_info.lines.count {|l| l.include?(?:) }
-    colornum != expected_colornum
+    cropped_page_img = cropped_img(page)
+    colornum         = cropped_page_img.color_histogram.count
+
+    cropped_page_img.destroy!
+
+    expected_colornum != colornum
   end
 
   def save_page(page, path)
-    `convert -density 92 -geometry #{width} "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
+    pdf_image_list[page].write(path)
   end
 
   def save_preview_page(page, path)
-    `convert -density 92 -geometry #{width} -fill "rgba(0, 255, 0, 50%)" -strokewidth 0 -draw "rectangle #{width - crop_width},#{height},#{width},0" "#{@pdf_path.shellescape}[#{page.to_s.shellescape}]" #{path.shellescape}`
+    page_img = pdf_image_list[page].dup
+
+    draw_preview_frame(page).draw(page_img)
+
+    page_img.write(path)
+
+    page_img.destroy!
   end
 
   private
 
   # TODO provide a way to set these config values
 
-  def crop
-    "#{crop_width}x+#{width - crop_width}+0"
+  def cropped_img(page)
+    page_img = pdf_image_list[page]
+    x        = right_margin_width(page)
+    page_img.crop(page_img.columns - x, 0, x, page_img.rows)
   end
 
-  def crop_width
-    @crop_width
+  def draw_preview_frame(page)
+    Magick::Draw.new.tap {|draw|
+      page_img = pdf_image_list[page]
+      draw.fill = 'rgba(0, 255, 0, 50%)'
+      draw.stroke_width(0)
+      draw.rectangle(page_img.columns - right_margin_width(page), 0, page_img.columns, page_img.rows)
+    }
   end
 
   def expected_colornum
     1
   end
 
-  # FIXME
-  def height
-    10000
+  def pdf_image_list
+    @pdf_image_list ||= Magick::ImageList.new(@pdf_path)
   end
 
-  def width
-    640
+  def right_margin_width(page)
+    page_img = pdf_image_list[page]
+    (page_img.columns * percentage).to_i
   end
 end
